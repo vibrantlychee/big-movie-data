5bcb9837fe89d01874ec16a5e06c1a71573b8388
vibrantlychee <99370899+vibrantlychee@users.noreply.github.com>
2022-03-26 20:51:07 +1100

    finished exploratory data analysis

--- cleaning_tools.py
+++ cleaning_tools.py
@@ -2,12 +2,42 @@ import pandas as pd
 
 # to store methods to prepare data for running models
 
-# creates a flat dataframe based on any column
-def flatten(df: pd.DataFrame, col: str):
+# # creates a new one-hot encoded dataframe based on any column
+# def one_hot(df: pd.DataFrame, col: str):
+#     """
+#     INPUTS:
+#         df:     (pd.DataFrame) a dataframe
+#         col:    (str) the name of a column in df
+#     OUTPUTS:
+#                 (pd.DataFrame) a dataframe one-hot encoded according to col
+#     """
+#     # flatten data on col
+#     flat_df = flatten(df, col)
+    
+#     # compute one hot encoding using str.get_dummies
+#     return flat_df.set_index("movie_id")[col + "_name"].str.get_dummies().groupby(level=0).sum()
+    
+# creates a flat dataframe based on any column (except cast or crew)
+def flatten(df: pd.DataFrame, cols: list):
     """
     INPUTS:
         df:     (pd.DataFrame) a dataframe
-        col:    (str) the name of a column in df
+        cols:   (list) list of column names in df
+    OUTPUTS:
+                (pd.DataFrame) a dataframe flattened according to cols
+    """
+
+    for col in cols:
+        df = df.explode(col)
+    
+    return df
+
+# # creates a flat dataframe based on any column
+def flatten_people(df: pd.DataFrame, col: str):
+    """
+    INPUTS:
+        df:     (pd.DataFrame) a dataframe
+        col:    (str) cast or crew
     OUTPUTS:
                 (pd.DataFrame) a dataframe flattened according to col
     """
@@ -25,18 +55,19 @@ def flatten(df: pd.DataFrame, col: str):
     
     return pd.concat(ls_of_temps)
 
+
 # creates a new one-hot encoded dataframe based on any column
-def one_hot(df: pd.DataFrame, col: str):
+def one_hot(df: pd.DataFrame, cols: list):
     """
     INPUTS:
         df:     (pd.DataFrame) a dataframe
-        col:    (str) the name of a column in df
+        cols:   (list) list of column names in df
     OUTPUTS:
-                (pd.DataFrame) a dataframe one-hot encoded according to col
+                (pd.DataFrame) a dataframe one-hot encoded according to cols
     """
     # flatten data on col
-    flat_df = flatten(df, col)
+    flat_df = flatten(df, cols)
     
-    # compute one hot encoding using str.get_dummies
-    return flat_df.set_index("movie_id")[col + "_name"].str.get_dummies().groupby(level=0).sum()
-     
+    # one-hot encode flat_df using pd.get_dummies()
+    return pd.get_dummies(flat_df, columns=cols).groupby(flat_df.index).max()
+    
